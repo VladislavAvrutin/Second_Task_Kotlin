6e752ef416d523f3efb617fd11b4c2c9d69a4c99
VladislavAvrutin <v.avrutin@dunice.net>
2022-05-20 17:13:39 +0300

    work bottom navigation bar

--- app/src/main/java/com/example/second_task_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/second_task_kotlin/MainActivity.kt
@@ -2,48 +2,29 @@ package com.example.second_task_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.second_task_kotlin.databinding.ActivityMainBinding
-import com.example.second_task_kotlin.ui.main.HomeLiveStreamAdapter
-import com.example.second_task_kotlin.ui.main.HomeViewPagerAdapter
-import com.example.second_task_kotlin.ui.main.mockData
-import com.example.second_task_kotlin.ui.main.pagerData
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-    private val adapter = HomeLiveStreamAdapter()
-    private val viewPagerAdapter = HomeViewPagerAdapter()
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        adapter.updateData(mockData)
-        viewPagerAdapter.updatePager(pagerData)
-        setContentView(binding.root)
-        init()
-        viewPagerInit()
+        setContentView(R.layout.activity_main)
+
 
         val navView : BottomNavigationView = findViewById(R.id.bNav)
 
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        val navController = navHost.navController
+        navView.setupWithNavController(navController)
     }
 
-    private fun init(){
-        binding.apply {
-            liveStream.layoutManager = LinearLayoutManager(
-                applicationContext,
-                RecyclerView.HORIZONTAL,
-                false
-            )
-            liveStream.adapter = adapter
 
-        }
-    }
-
-    private fun viewPagerInit(){
-        binding.pager.adapter = viewPagerAdapter
-    }
 
 }
--- app/src/main/java/com/example/second_task_kotlin/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/second_task_kotlin/ui/main/MainFragment.kt
@@ -5,19 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.second_task_kotlin.R
+import com.example.second_task_kotlin.databinding.FragmentMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(R.layout.fragment_main) {
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    private lateinit var binding : FragmentMainBinding
+   private val adapter = HomeLiveStreamAdapter()
+   private val viewPagerAdapter = HomeViewPagerAdapter()
+
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMainBinding.bind(view)
+
+        adapter.updateData(mockData)
+        viewPagerAdapter.updatePager(pagerData)
+
+        init()
+
+        viewPagerInit()
     }
 
 
+
+    private fun init(){
+        binding.apply {
+            liveStream.layoutManager = LinearLayoutManager(
+                context,//земена applicationcontext
+                RecyclerView.HORIZONTAL,
+                false)
+            liveStream.adapter = adapter
+
+        }
+
+    }
+
+    private fun viewPagerInit(){
+        binding.pager.adapter = viewPagerAdapter
+    }
 }
+
